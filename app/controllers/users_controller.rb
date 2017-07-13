880362709e9380b328cc174896fca21f948dc34a
Daniel Myers <daniel.p.myers@gmail.com>
2017-07-13 17:37:20 +0000

    Add payment.js with Stripe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,18 +16,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
-    if @user.save
+    if @user.valid? && valid_card_charge
+      @user.save
       session[:user_id] = @user.id
       AppMailer.welcome(current_user).deliver
       flash[:success] = "Account created!"
-      Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-      token = params[:stripeToken] || "tok_visa"
-      charge = Stripe::Charge.create(
-        :amount => 999,
-        :currency => "usd",
-        :description => "#{@user.email} Sign up charge",
-        :source => token,
-      )
       redirect_to home_path
     else
       render 'new'
@@ -38,6 +31,26 @@ class UsersController < ApplicationController
   
     def user_params
       params.require('user').permit('email', 'password', 'full_name')
+    end
+    
+    def valid_card_charge
+      Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+      token = Rails.env.test? ? "tok_visa" : params[:stripeToken]
+      begin
+        charge = Stripe::Charge.create(
+          :amount => 999,
+          :currency => "usd",
+          :description => "#{@user.email} Sign up charge",
+          :source => token,
+        )
+        return true
+      rescue Stripe::CardError => e
+        flash[:danger] = e.message
+        return false
+      rescue => e
+        flash[:danger] = "Some error occurred."
+        return false
+      end
     end
     
 end
